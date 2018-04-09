9e05b9db4168eecde33a2a011df4cbc0271ccfec
JakubWagner <jakub.wagner.1019@gmail.com>
2018-04-09 13:17:14 +0200

    separated SQL management from main file

--- managerSQL.py
+++ managerSQL.py
@@ -1,15 +1,17 @@
 #_managerSQL
 
 from tkinter import *
-import sqlite3
+from tkinter import filedialog
 import qRegister as qr
+import SQLmanager as sql
+
 
 def sendQuery():
- c.execute(entry.get())
- qreg.addQuery(entry.get())
+ query=entry.get()
+ result=sqlManager.execute(query)
+ qreg.addQuery(query)
  output.delete('1.0',END)
- output.insert(END,c.fetchall())
- conn.commit()
+ output.insert(END,result)
  entry.delete(0,END)
  
 def sendQuery2(event):
@@ -22,15 +24,24 @@ def getOlder(event):
 def getNewer(event):
  entry.delete(0,END)
  entry.insert(0,qreg.getNewerQuery()) 
+
+def establishConnection():
+ file = filedialog.askopenfilename(filetypes=(("Databases","*.db"),("All files","*.*")))
+ output.delete('1.0',END)
+ sqlManager.reconnect(file)
+ output.insert(END,"Established a new connection"+file)
  
-
-  
-conn = sqlite3.connect('data.db')
-c=conn.cursor()
-
+sqlManager=sql.SQLmanager('data.db')
 qreg=qr.QueryRegister()
 
 root=Tk()
+
+menu=Menu(root)
+root.config(menu=menu)
+subMenu=Menu(menu)
+menu.add_cascade(label="Connection",menu=subMenu)
+subMenu.add_command(label="Establish a connection...",command=establishConnection)
+
 frame1=Frame(root)
 label=Label(frame1,text='query')
 entry=Entry(frame1,width=100)
@@ -41,15 +52,11 @@ root.bind("<Escape>",quit)
 root.bind("<Up>",getOlder)
 root.bind("<Down>",getNewer)
 
-
 label.pack(side=LEFT)
 entry.pack(side=LEFT)
 button.pack(side=LEFT)
 frame1.pack(side=TOP)
 output.pack(side=BOTTOM,fill=X)
 
-pastQueries=[]
-
-
 root.mainloop()
-conn.close()
+sqlManager.close()
