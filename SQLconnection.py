1dd9155cf9f1c4fa4d4f49e4a77e3cda90a4b9f6
JakubWagner <jakub.wagner.1019@gmail.com>
2018-04-13 09:07:33 +0200

    separated SQL management

---
+++ SQLconnection.py
@@ -0,0 +1,20 @@
+#_SQLmanager
+
+import sqlite3
+
+class SQLconnection():
+ def __init__(self,file):
+  self.connection=sqlite3.connect(file)
+  self.cursor=self.connection.cursor()
+ def reconnect(self,file):
+  self.connection.close()
+  self.connection=sqlite3.connect(file)
+  self.cursor=self.connection.cursor()
+ def execute(self,query):
+  self.cursor.execute(query)
+  result=self.cursor.fetchall()
+  self.connection.commit()
+  return result
+ def close(self):
+  self.connection.close()
+  
